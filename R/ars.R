ef737dd795a6439b4d470c2ed8d43402e9c2dc9f
jiahui-zhao <jiahui_zhao@berkeley.edu>
2020-12-09 14:28:46 -0800

    add detail 1

--- R/ars.R
+++ R/ars.R
@@ -67,73 +67,127 @@ ars <- function(n,
     return(log(fun(x, ...)))
   }
   
+  
+  
   # taking derivative
   dh <- function(x, h, dx = 1e-8) {
     return((h(x+dx)-h(x))/dx)
   }
   
-  zk <- function(x1, x2, h, dh) {
-    return((h(x2)-h(x1)-x2*dh(x2)+x1*h(x1))/(dh(x1)-dh(x2)))
+  
+
+  cal_zk <- function(x, h, dh) {
+    tmp = c()
+    for (j in 1:(length(x)-1)) {
+      zj = (h(x[j+1])-h(x[j])-x[j+1]*dh(x[j+1])+x[j]*h(x[j]))/(dh(x[j])-dh(x[j+1]))
+      tmp = append(tmp, zj)
+    }
+    return(tmp)
   }
   
-  uk <- function(x, h, dh) {
-    return(list(slope = dh(x), intercept = h(x) - x*dh(x)))
+  
+  
+  cal_uk <- function(x, h, dh) {
+    m_tmp = c()
+    int_tmp = c()
+    for (j in 1:length(x)) {
+      m_tmp = append(m_tmp, dh(x[j]))
+      int_tmp = append(int_tmp, h(x[j] - x[j]*dh(x[j])))
+    }
+    return(list(slope = m_tmp, intercept = int_tmp))
   }
   
-  lk <- function(x1, x2, h) {
-    return(list(slope = (h(x2)-h(x1))/(x2-x1),
-                intercept = (x2*h(x1)-x1*h(x2))/(x2-x1)))
+  
+  
+  cal_lk <- function(x, h) {
+    m_tmp = c()
+    int_tmp = c()
+    for (j in 1:(length(x)-1)) {
+      m_tmp = append(m_tmp, (h(x[j+1])-h(x[j]))/(x[j+1]-x[j]))
+      int_tmp = append(int_tmp, (x[j+1]*h(x[j])-x[j]*h([j+1]))/(x[j+1]-x[j]))
+    }
+    return(list(slope = m_tmp,
+                intercept = int_tmp))
   }
   
-  ### set starting values of abscissaes ###
   
-  if (lower == -Inf && upper != Inf) {
+  
+  ## ************************* Check Boundary & Set Initial abscissae **************** ###
+  
+  if (lower == -Inf && upper != Inf) { # valid upper, invalid lower
     
     start = upper
     while(dh(start) <= 0) { start = start - step }
     x0 = c(start, upper)
     
-  } else if (l != -Inf && u == Inf) {
+  } else if (l != -Inf && u == Inf) { # valid lower, invalid upper
     
     start = lower
     while(dh(start) >= 0) { start = start + step }
     x0 = c(lower, start)
     
-  } else if (l == -Inf && u == Inf) {
+  } else if (l == -Inf && u == Inf) { # both lower, upper invalid
     
     startl = 0 - step
     startu = 0 + step
-    while(dh(start1) <= 0) { startl = startl - step }
+    while(dh(start1) <= 0) { startl = startl - step } 
     while(dh(startu) >= 0) { startu = startu + step }
     x0 = c(startl, startu)
     
-  } else {
+  } else {                            # both lower, upper valid
+    
     x0 = c(lower, upper)
   }
   
-  x_abscissae = seq(x0[1], x0[2], length.out = k) # initial abscissae, with k = 2 by default
+  x_abscissae = seq(x0[1], x0[2], length.out = k+2)[2:(k+1)] # avoid two boundary points
+                                                             # use interior points as abscissae
+                                                  
   
+  ##******************************** Main Function **********************************##
   
-  ##*************************** Sampling Loop, Main Function ******************************##
-  
+  ## initialize variables that will be updated during iteration
   
   samples <- c()
-  ite <- 0
-  reinitialize <- FALSE
+  zk <- cal_zk(x_abscissae, h, dh)
+  uk_slope <- cal_uk(x_abscissae, h, dh)[[1]]
+  uk_intercept <- cal_uk(x_abscissae, h, dh)[[2]]
+  lk_slope <- cal_lk(x_abscissae, h)[[1]]
+  lk_intercept <- cal_lk(x_abscissae, h)[[2]]
+  update <- FALSE
+  
+  ### Iteration ###
   
   while(length(samples) < n) {
     
-    ###  Updating step, when x* in added Tk+1
-    if (reinitialize == TRUE) {
+    ### sampling step
+    x_star = get_sample()  ## draw sample from sk
+    w = runif(1, 0, 1)
+    
+    
+    if (w <= exp()) {                   # match by first condition, l - u
+        samples = append(samples, x_star)
       
-      x_vec <- sort(c(x, newx), decreasing = FALSE, index.return=TRUE)
+      } else if (w <= exp(h(x_star) - )) {       # match by second condition, h - u
+          samples = append(samples, x_star)
+          update = TRUE
       
+      }
+    
+    ###  Updating step, when x* is added (Tk --> Tk+1)
+    if (update == TRUE) {
+      
+      # update x values
+      x_abscissae <- sort(c(x_abscissae, x_star))
+      
+      # update all relevent variables
+      zk <- cal_zk(x_abscissae, h, dh)
+      uk_slope <- cal_uk(x_abscissae, h, dh)[[1]]
+      uk_intercept <- cal_uk(x_abscissae, h, dh)[[2]]
+      lk_slope <- cal_lk(x_abscissae, h)[[1]]
+      lk_intercept <- cal_lk(x_abscissae, h)[[2]]
+      update <- FALSE
+      }
     }
-    
-    ### Sampling step
-    
-    
-  }
   
   return(samples)
 }
