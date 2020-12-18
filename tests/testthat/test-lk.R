e1deb4353d5c55d58d166f85a43909811a61d439
eric-leu-chu <eric.leu.chu@gmail.com>
2020-12-18 09:20:01 -0600

    Added a little more description to the test

--- tests/testthat/test-lk.R
+++ tests/testthat/test-lk.R
@@ -3,11 +3,11 @@
 # Test calc_lk function.
 test_that("Check class and length of calc_lk outputs", {
 
-  print("Checking the class and length of results returned by calc_lk")
   g <- dnorm
   h <- function(x) log(g(x))
   Tk <- c(-1,1)
 
+  print("Checking the class and length of results returned by calc_lk")
   expect_equal(class(calc_lk(h, Tk)), "list")
   expect_equal(class((calc_lk(h, Tk))[[1]]), "function")
   expect_equal(length(calc_lk(h, Tk)), length(Tk)-1)
@@ -21,6 +21,7 @@ test_that("Check if the sampled x star value is outside the abscissae returns -I
   Tk <- c(-1,1)
   lks <- calc_lk(h, Tk)
 
+  print("Checking to see if the function returns -Inf when x-star is outside the Tk abscissae")
   expect_equal(get_lk_x(x=-10, Tk=c(-1,1), lks),-Inf)
   expect_false(get_lk_x(x=0, Tk=c(-1,1), lks) == -Inf)
   expect_equal(class(get_lk_x(x=0, Tk=c(-1,1), lks)), "numeric")
