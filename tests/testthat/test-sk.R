dda25fc8ba68be1e74f0872b17ac1c75ac120e4e
eric-leu-chu <eric.leu.chu@gmail.com>
2020-12-18 11:10:09 -0600

    Updated testing. Works now.

--- tests/testthat/test-sk.R
+++ tests/testthat/test-sk.R
@@ -1,12 +1,13 @@
 
 # Test sample_sk function in sk.R file.
 
-test_that("Return NA if length(Tk) is greater than length(z),
+test_that("Return error if the number of abscisscae is not one less than the length of Z, the number of intervals,
           the number of intervals", {
 
   g <- dnorm
   h <- function(x) log(g(x))
 
-  # expect_true(is.na(sample_sk(Tk=c(-1,0, 1), z=c(-Inf, 0, Inf), h, exp_uks)))
-  # expect_false(is.na(sample_sk(Tk=c(-1, 1), z=c(-Inf, 0, Inf), h, exp_uks)))
+  expect_error(sample_sk(Tk=c(-1,0,1), z=c(-Inf, 0, Inf), h, exp_uks))
+  expect_error(sample_sk(Tk=c(-1, 0, 1, 2), z=c(-Inf, 0, 4, Inf), h, exp_uks))
+  expect_equal(class(sample_sk(Tk=c(-1, 1), z=c(-Inf, 0, Inf), h, exp_uks)), "numeric")
 })
