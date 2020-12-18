acecbe201f89111a2197abb2b662cf351eaea448
jiahui-zhao <jiahui_zhao@berkeley.edu>
2020-12-17 22:22:01 -0800

    fix some error

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,5 +1,3 @@
-library(pracma)
-library(truncnorm)
 
 # Test fderiv function.
 test_that("Check cases for different distributions.", {
@@ -15,8 +13,8 @@ test_that("Check that the derivatives for initial points are correct for the unb
   norm_dist <- function(x){dnorm(x, mean=5)}
   t_dist <- function(x){dt(x, df = 4)}
   gamma_dist <- function(x){dgamma(x, shape = 5, rate=3)}
-  f_dist <- function(x){df(x, df1 = 10, df2 = 3)}
   trunc_norm_dist <- function(x){dtruncnorm(x, a=-Inf, b=0)}
+  f_dist <- function(x){df(x, df1 = 10, df2 = 3)}
   beta_dist <- function(x){dbeta(x, shape1=2, shape2=3)}
 
   print("Checking negative infinity to infinity case")
@@ -37,30 +35,47 @@ test_that("Check that the derivatives for initial points are correct for the unb
 })
 
 
-# get_uk_x
-# get_lk_x
-
-#################  z  #########################
-
 test_that("Check Length Consistency of z", {
   print("Z is calculated properly.")
+
+  g = dnorm
+  h = function(x) log(g(x))
+  Tk = c(-1,1)
+  bounds = c(-2, 2)
+  z = calc_z(bounds, Tk, h)
+
   expect_equal(length(z), 1+length(Tk))
 })
 
-test_that("General Functions are correct", {
+test_that("Distributions are correct", {
+
+  ptrunc <- function(x, spec, a = -Inf, b = Inf, ...)
+  {
+    tt <- x
+    aa <- rep(a, length(x))
+    bb <- rep(b, length(x))
+    G <- get(paste("p", spec, sep = ""), mode = "function")
+    tt <- G(apply(cbind(apply(cbind(x, bb), 1, min), aa), 1, max), ...)
+    tt <- tt - G(aa, ...)
+    tt <- tt/(G(bb, ...) - G(aa, ...))
+    return(tt)
+  }
+
+  # the ptrunc function above is adapted from
+  # https://github.com/boyinggong/Adaptive-rejection-sampling/blob/master/ars/tests/testthat/test-main.R
 
   print("test normal and truncated normal with mean 0 variance 1")
   expect_true(ks.test(ars(dnorm, c(-Inf,Inf), n = 1000), pnorm)$p.value > 0.05)
-  expect_true(ks.test(ars(dnorm, c(-1,1), n = 1000), function(x) {ptrunc(x, "norm", a = -1, b = 1)})$p.value > 0.05)
-  expect_true(ks.test(ars(dnorm, c(-Inf,1), n = 1000), function(x){ptrunc(x, "norm", a = -Inf, b = 1)})$p.value > 0.05)
-  expect_true(ks.test(ars(dnorm, c(1,Inf), n = 1000), function(x){ptrunc(x, "norm", a = 1, b = Inf)})$p.value > 0.05)
+  expect_true(ks.test(ars(dnorm, c(-1,1), n = 1000), function(x) {ptrunc(x, "norm", a = -1, b = 1)})$p.value > 0.1)
+  expect_true(ks.test(ars(dnorm, c(1,Inf), n = 1000), function(x){ptrunc(x, "norm", a = 1, b = Inf)})$p.value > 0.1)
 
-  print("test Chi square and truncated Chi square with df > 2")
-  expect_true(ks.test(ars(dchisq, c(0,Inf), n = 1000, df = 3), function(x) {ptrunc(x, "chisq", a = 0, b = Inf, df=3)})$p.value > 0.05)
-  expect_true(ks.test(ars(dchisq, c(1,4), n = 1000, df = 3), function(x) {ptrunc(x, "chisq", a = 1, b = 4, df=3)})$p.value > 0.05)
+  print("test gamma distribution with shape = 5 and rate = 3")
+  gamma_dist <- function(x){dgamma(x, shape = 5, rate=3)}
+  expect_true(ks.test(ars(g=gamma_dist, n=1000, initial = NULL, bounds = c(0, Inf)),
+                      function(x) {ptrunc(x, "gamma", a = 0, b = Inf, shape = 5, rate=3)})$p.value > 0.1)
 
-  print("test Beta and truncated Beta with df1 > 1 and df2 > 1")
-  expect_true(ks.test(ars(dbeta, c(0,1), n = 1000, shape1 = 2, shape2 = 2), function(x) {pbeta(x, 2, 2)})$p.value > 0.05)
-  expect_true(ks.test(ars(dbeta, c(0,1/2), n = 1000, shape1 = 2, shape2 = 2), function(x) {ptrunc(x, "beta", a = 0, b = 1/2, 2, 2)})$p.value > 0.05)
-  expect_true(ks.test(ars(dbeta, c(1/4,3/4), n = 1000, shape1 = 2, shape2 = 2), function(x) {ptrunc(x, "beta", a = 1/4, b = 3/4, 2, 2)})$p.value > 0.05)
-})
+  print("test t and truncated t with degree of freedom 2")
+  t_dist <- function(x) {dt(x, df = 2)}
+  expect_true(ks.test(ars(g=t_dist, n=1000, initial = NULL, bounds = c(0, Inf)), function(x) {ptrunc(x, "t", a = 0, b = Inf, df=2)})$p.value > 0.1)
+  expect_true(ks.test(ars(g=t_dist, n=1000, initial = NULL, bounds = c(-Inf, Inf)), function(x) {ptrunc(x, "t", a = -Inf, b = Inf, df=2)})$p.value > 0.1)
+  })
