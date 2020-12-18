8071ca7489f6d0b5201cf66c10f4f70007d13087
jiahui-zhao <jiahui_zhao@berkeley.edu>
2020-12-17 22:26:33 -0800

    fix errors

--- tests/testthat/test-sk.R
+++ tests/testthat/test-sk.R
@@ -1,7 +1,11 @@
 # Test sample_sk function in sk.R file.
 
-test_that("Return NA if length(Tk) is greater than length(z), the number of intervals", {
+test_that("Return NA if length(Tk) is greater than length(z),
+          the number of intervals", {
+
+  g <- dnorm
+  h <- function(x) log(g(x))
+
   expect_true(is.na(sample_sk(Tk=c(-1,0, 1), z=c(-Inf, 0, Inf), h, exp_uks)))
   expect_false(is.na(sample_sk(Tk=c(-1, 1), z=c(-Inf, 0, Inf), h, exp_uks)))
 })
-  
--- tests/testthat/test-uk.R
+++ tests/testthat/test-uk.R
@@ -3,11 +3,11 @@
 # Test calc_uk function.
 test_that("Check class and length of calc_uk outputs", {
   print("Checking classes of results returned by calc_uk")
-  
+
   g <- dnorm
   h <- function(x) log(g(x))
   Tk <- c(-1,1)
-  
+
   expect_equal(class(calc_uk(h, Tk, F)), "list")
   expect_equal(class((calc_uk(h, Tk, F))[[1]]), "function")
   expect_equal(class((calc_uk(h, Tk, F))[[2]]), "function")
@@ -16,9 +16,10 @@ test_that("Check class and length of calc_uk outputs", {
 
 # Test get_uk_x function.
 test_that("Throw error if domain specification of z is not valid for the given x.", {
-  
+
+  Tk <- c(-1,0,1)
   uks <- calc_uk(h, Tk, F)
-  
+
   expect_error(get_uk_x(x=-15, z=c(-10,0,10), uks))
   expect_error(get_uk_x(x=15, z=c(-10,0,10), uks))
   expect_equal(class(get_uk_x(x=5, z=c(-10,0,10), uks)), "numeric")
